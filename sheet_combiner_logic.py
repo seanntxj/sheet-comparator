b0760738ab55d0f0e3276c1d5e646785b37f8cdc
seanntxj <seanntxj@gmail.com>
2024-07-30 18:03:54 +0800

    Learning Pandas, sheet combiner

--- sheet_combiner_logic.py
+++ sheet_combiner_logic.py
@@ -1,48 +1,53 @@
-import csv 
-import sys
-import os.path
-import time
-from enum import Enum
-from openpyxl import Workbook, styles, load_workbook
-import threading
-from queue import Queue, Empty
-from common_logic import xlsx_to_csv
+import pandas as pd
+import Levenshtein
 
-class Sheet:
-    def __init__(self) -> None:
-        self.fields: dict[str] = {} # field_val:column_index
-        self.data: list[str] = []
+def merge_dfs(df1, df2, df1_identifier_column, df2_identifier_column):
+    '''
+    Merge two Pandas Dataframes
+    Keep all rows from the first dataframe, only keep the first dataframe's copy of a column which appears in both dataframes.
+    If the identifier column of both dataframes are different, only the first dataframe's copy is kept.
+    '''
+    # Rename the identifier column of the second dataframe to be the same of the first. Avoids having both columns kept in the merged dataframe.
+    df2 = df2.rename(columns={df2_identifier_column: df1_identifier_column}) 
+    # Left merge the two dataframes. Suffix the second dataframe's identifier column in case.
+    resulting_df = pd.merge(df1, df2, how='left', left_on=df1_identifier_column, right_on=df1_identifier_column, suffixes=('', '_removeMe')) 
+    return resulting_df
 
+def find_nearest_string(target, list_of_strings):
+    """
+    Finds the closest string in a list of strings with a combination of the Levenshtein distance and string size consistency.
+    Closest string to the target is the least number of substitutions in a potential string INCLUDING removal of characters.
+    """
 
-def combine_sheets(fields: list[str] = [],
-                   primary_sheet: Sheet = None,
-                   sheets: list[Sheet] = []) -> None:
-    
-    master_output_sheet = Sheet()
-    master_output_sheet.fields = fields
-    master_output_sheet.data = [fields]*len(primary_sheet.data)
-    sheets.insert(0, primary_sheet)
- 
-    for sheet in sheets:
-        # For every col in the final sheet
-        for col in fields:
-            # Find the same row in current sheet
-            idx_col_current_sheet = sheet.fields[col]
-            for idx_row_master_sheet in range(len(master_output_sheet.data)):
-                x = sheet.data[idx_col_current_sheet][idx_row_master_sheet] 
-    return 
+    # Check for exact match first
+    list_of_strings = [item.lower() for item in list_of_strings]
+    target = target.lower()
+    if target in list_of_strings:
+        return target
 
+    max_len_of_str = max(len(item) for item in list_of_strings)
 
+    score = []
+    for potential_target in list_of_strings:
+        size_matched_potential_target = f"{potential_target}{(max_len_of_str - len(potential_target)) * '~'}"  # Append string to be the same size as the max sized potential match
+        similarity = Levenshtein.distance(size_matched_potential_target, target)
+        score.append((similarity, potential_target))
+
+    # Find the index of the minimum score
+    min_index = min(range(len(score)), key=lambda i: score[i][0])
+
+    # If there are multiple strings with the same minimum score, choose the one closer in length to the target
+    min_score = score[min_index][0]
+    candidates = [string for similarity, string in score if similarity == min_score]
+    closest_length = min(abs(len(target) - len(string)) for string in candidates)
+    best_match = next(string for string in candidates if abs(len(target) - len(string)) == closest_length)
+
+    return best_match
 
 if __name__ == "__main__": 
-    x = Sheet()
-    x.fields = {'test1': 0, 'test2': 1, 'test3': 2}
-    x.data = [['data1', 'd2', 'd3'], ['dx1', 'fx2']]
-
-    y = Sheet()
-    y.fields = {'dee1': 0, 'dee2': 1, 'dee3': 2}
-    y.data = [['aaaata1', 'aaa2', 'aaa3'], ['aaax1', 'fx2']]
-
-    f = ['test1', 'test2', 'test3', 'test4']
-    combine_sheets(f, x, [y])
+    # Load all dataframes into memory 
+    # Establish which dataframe is the primary
+    # Establish which dataframes have what columns
+    # Using the primary dataframe, start to merge with the others 
+    # Return the merged dataframe
     pass
